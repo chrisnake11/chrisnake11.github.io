21946dc5d8d94a3f4adb28b3750f0d054d17b6a8
zheyv <zyvxiang@163.com>
2024-04-18 14:22:24 +0800

    update picture

--- src/config.ts
+++ src/config.ts
@@ -32,8 +32,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'Zheyv',
+  bio: '',
   links: [
     {
       name: 'Twitter',
@@ -48,7 +48,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/chrisnake11',
     },
   ],
 }
