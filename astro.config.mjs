9b8426f299f644a73ed6d1eeee9fd97fcec38ed0
zheyv <2500517199@qq.com>
2025-06-10 23:33:09 +0800

    build blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://chrisnake11.github.io",
+	base: "chrisnake11.github.io",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
