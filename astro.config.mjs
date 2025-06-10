94af16a5a915828143d4ae8f965af576373f9611
zheyv <2500517199@qq.com>
2025-06-10 23:57:14 +0800

    build blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://chrisnake11.github.io",
-	base: "chrisnake11.github.io",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
